e9f27b29814a835bb31b46cec846455d0ee606f9
“nikolaiKudriashov” <kolyasolo322@gmail.com>
2023-01-25 21:59:48 +0300

    refactoring fix cli.js

--- src/games/cli.js
+++ src/games/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-const userName = () => {
+const askUserName = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
