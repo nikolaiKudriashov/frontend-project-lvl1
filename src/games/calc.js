76602688126e61a858593bf8e2c5cdb8e9b0b2eb
“nikolaiKudriashov” <kolyasolo322@gmail.com>
2023-01-29 17:17:10 +0300

    refactoring fix calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -4,6 +4,12 @@ import getRandomNumber from '../utils.js';
 
 const description = 'What is the result of the expression?';
 
+const getOperator = () => {
+  const operators = ['+', '-', '*'];
+  const operatorNumber = getRandomNumber(0, 2);
+  return operators[operatorNumber];
+}
+
 const generateRound = () => {
   const number1 = getRandomNumber(1, 100);
   const number2 = getRandomNumber(1, 100);
@@ -12,16 +18,16 @@ const generateRound = () => {
   let question;
   let correctAnswer;
 
-  switch (operator) {
-    case 1:
+  switch (getOperator()) {
+    case '+':
       question = `${number1} + ${number2}`;
       correctAnswer = String(number1 + number2);
       break;
-    case 2:
+    case '-':
       question = `${number1} - ${number2}`;
       correctAnswer = String(number1 - number2);
       break;
-    case 3:
+    case '*':
       question = `${number1} * ${number2}`;
       correctAnswer = String(number1 * number2);
       break;
