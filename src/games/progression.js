a6b3fdde3ea24708afd76e3e63350e2d41c14cb8
“nikolaiKudriashov” <kolyasolo322@gmail.com>
2023-01-29 17:54:42 +0300

    refactoring fix progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,18 +3,22 @@ import getRandomNumber from '../utils.js';
 
 const description = 'What number is missing in the progression?';
 
-const generateRound = () => {
-  const progressionLength = getRandomNumber(5, 10);
-  const startNum = getRandomNumber(1, 20);
-  const progressionDiff = getRandomNumber(1, 10);
-  const hiddenElementIndex = getRandomNumber(0, progressionLength - 1);
-
-  let question = '';
-  const correctAnswer = String(startNum + progressionDiff * hiddenElementIndex);
-
-  for (let i = 0; i < progressionLength; i += 1) {
-    question += i === hiddenElementIndex ? '.. ' : `${startNum + i * progressionDiff} `;
+const generateQuestion = (start, step, length, hidden) => {
+  let result = '';
+  for (let i = 0; i < length; i += 1) {
+    result += i === hidden ? '.. ' : `${start + i * step} `;
   }
+  return result;
+};
+
+const generateRound = () => {
+  const progLength = getRandomNumber(5, 10);
+  const startNum = getRandomNumber(1, 20);
+  const progDiff = getRandomNumber(1, 10);
+  const hiddenElementIndex = getRandomNumber(0, progLength - 1);
+
+  const correctAnswer = String(startNum + progDiff * hiddenElementIndex);
+  const question = generateQuestion(startNum, progDiff, progLength, hiddenElementIndex);
 
   return [question, correctAnswer];
 };
